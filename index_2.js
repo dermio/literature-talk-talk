a288865439660aa91c4e59ed7e9c21deeb32e051
dermio <thisdjay@gmail.com>
2017-08-11 16:05:14 -0700

    add JS and youtube API index_2.js

---
+++ index_2.js
@@ -0,0 +1,100 @@
+// ThinkDive API and endpoint info
+const API_KEY_TASTEDIVE = '280012-Booksand-1ZFHAWKT';
+const ENDPOINT_TASTEDIVE = 'https://tastedive.com/api/similar';
+
+// YouTube API and endpoint info
+const API_KEY_YOUTUBE = 'AIzaSyDf9VtvE5wTSJyzYAfmWV6GJd_vzSa3r2w';
+const ENDPOINT_YOUTUBE = 'https://www.googleapis.com/youtube/v3/search';
+const YOUTUBE_WATCH_VID = 'https://www.youtube.com/watch?v=';
+
+
+const MY_DATA = {
+	tastedive: null,
+	youtube: null
+};
+
+/*
+function displaySearchResults(jsonTasteDive, jsonYouTube) {
+  return `<>from TasteDive API<>
+          <>from TasteDive API<>
+          <>from YOUTUBE API<>
+          <>from TasteDive API<>`
+}
+*/
+
+
+function showData(param) {
+	console.log(MY_DATA);
+}
+
+// The functions below grab the data from their respective API calls
+// and store it in an object MY_DATA
+
+function getDataFromAPI(searchTerm) {
+  // data sent with $.getJSON() to YouTube
+  let dataYouTubeAPI = {
+		part: 'snippet', // part: 'snippet' required by YouTube data API
+		key: API_KEY_YOUTUBE,
+		q: searchTerm,
+		maxResults: 5
+	};
+	
+	// data sent with $.ajax() to TasteDive
+	let dataTasteDiveAPI = {
+		k: API_KEY_TASTEDIVE,
+		q: searchTerm,
+		//type: 'books',
+		limit: 10,
+
+		info: 1 // extra, verbose=1
+	};
+
+	// Two .ajax() queries passed to when().
+	// After data is sent back form both .ajax() queries
+	// the callback function in then() will call showData(MY_DATA)
+  $.when(
+	  // $.getJSON() call for YouTube API
+	  $.getJSON(ENDPOINT_YOUTUBE, dataYouTubeAPI, function (data) {
+		  MY_DATA.youtube = data;
+		}),
+		
+		// $.ajax() call for TasteDive API
+	  $.ajax({
+		  type: 'GET',
+		  url: ENDPOINT_TASTEDIVE,
+
+		  jsonp: 'callback',  // get around CORS
+		  dataType: 'jsonp',  // get around CORS
+		  data: dataTasteDiveAPI, // defined above
+
+		  success: function (data) {
+		   	MY_DATA.tastedive = data;
+		  }
+		})
+
+	).then(function () {
+		showData(MY_DATA);
+	});
+  
+  //console.log(dataFromYouTube);
+}
+
+function watchSubmit() {
+  $('.submitBookSearch').on('submit', function (event) {
+    event.preventDefault();
+    
+		let queryTarget = $(this).find('#searchBook'); // input field
+		let queryBook = queryTarget.val(); // get the book value entered
+		//console.log(queryBook);
+		
+		queryTarget.val('');
+    
+    console.log(queryBook);
+
+    getDataFromAPI(queryBook); // function call
+  });
+}
+
+$(watchSubmit);
+
+/////////
